0c9e33af8145bf8cecd47036c7c28bc92c729b7f
DannyHuacon <55764753+DannyHuacon@users.noreply.github.com>
2019-10-01 14:33:27 -0500

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Realizar un programa en C que determine el area de un circulo
+*******************************************************************************/
+#include <stdio.h>
+#include <math.h>
+#define pi 3.1416
+
+void main()
+{
+    float radio,area=0;
+    printf("ingrese el radio");
+    scanf("%f",&radio);
+    //area=pi*radio*radio;
+    area=pi*pow(radio,2);
+    printf("el area es %.2f",area);
+    
+    
+}
